ed273f384c8dcc8e959fb5884853357e84e65daf
morteza80 <morteza.3921@gmail.com>
2022-06-21 02:46:31 +0430

    for alirea

---
+++ src/pages/AvdancedSearch/AvdancedSearch.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './AvdancedSearch.css'
+
+function AvdancedSearch() {
+  return (
+    <div className='AvdancedSearch'>
+        <div className='AvdancedSearchTop1'>
+  
+        </div>
+        <div className='AvdancedSearchTop2'>
+
+        </div>
+        <div className='AvdancedSearchBody'>
+          
+        </div>
+      
+    </div>
+  )
+}
+
+export default AvdancedSearch
